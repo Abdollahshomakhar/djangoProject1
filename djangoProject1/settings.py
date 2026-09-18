f6278016e77a77453c58ba1b9a478d46e22f4771
Esi <eghorbani361@gmail.com>
2026-09-18 10:13:08 +0330

    Update settings.py

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#22pcpd)#!g=doq!f(q&^qe9hzval&vnge#3rrvlz0zsc%d7-5'
-
+SECRET_KEY = os.environ.get(
+    'DJANGO_SECRET_KEY',
+    'django-insecure-development-only-key'
+)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
