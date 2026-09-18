9f385a3a76bfc27d8de8faa0948b34afd8dc7fe1
Esi <eghorbani361@gmail.com>
2026-09-18 10:19:32 +0330

    Update settings.py

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -20,10 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(
-    'DJANGO_SECRET_KEY',
-    'django-insecure-development-only-key'
-)
+import os
+
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
